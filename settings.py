bda9c55f1c821ba5f86e75fa28583f07f31ac4b2
Kennedy Mwangi <85018677+Exclema@users.noreply.github.com>
2025-03-30 16:59:00 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+INSTALLED_APPS = [
+    ...,
+    'obituaries',
+]
